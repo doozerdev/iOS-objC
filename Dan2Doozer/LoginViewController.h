8372d445a336239041410b3f87ace4a88976a534
Dan Apone <dapone@gmail.com>
2015-05-03 16:31:25 -0700

    updated product name and bundle identifier

---
+++ Dan2Doozer/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Dan2Doozer
+//
+//  Created by Daniel Apone on 5/3/15.
+//  Copyright (c) 2015 Daniel Apone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
